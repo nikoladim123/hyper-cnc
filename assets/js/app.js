19978a95e5e109740ef0e311cadc295d40d06b11
unknown <nikoladimitrijevic1991@gmail.com>
2020-08-05 15:46:56 +0200

    airsoft section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
+// header anims
 var galleryBoxContent = document.getElementsByClassName('galleryBoxContent')
 var galleryHeading = document.getElementsByClassName('galleryHeading')
-
 
 function servicesHeadingFun() {
   if(servicesHeading[0].getClientRects()[0].top < window.innerHeight /1.2){
@@ -26,6 +26,26 @@ function galleryBoxContentShowText() {
 }
 
 
+// airsoft section anims
+
+
+var airSoftSection = document.getElementsByClassName('airSoftSection')
+var airsoftOverlay = document.getElementsByClassName('airsoftOverlay')
+var airsoftHeading = document.getElementsByClassName('airsoftHeading')
+
+function airSoftSectionFun() {
+  if(airSoftSection[0].getClientRects()[0].top < window.innerHeight /1.5){
+    airsoftOverlay[0].style.background = 'rgba(0,0,0,0)';
+    airsoftHeading[0].style.color = 'black';
+  }else{
+    airsoftOverlay[0].style.background = 'rgba(0,0,0,0.9)';
+    airsoftHeading[0].style.color = 'white';
+  }
+}
+
+
+// windows events
 window.addEventListener('scroll',()=>{
   galleryBoxContentShowText()
+  airSoftSectionFun()
 })
